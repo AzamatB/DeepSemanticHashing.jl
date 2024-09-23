d52debce50a62c330cf4df9c8b377e71d773b043
Azamat Berdyshev <aberdysh@gmail.com>
2024-09-23 11:17:59 +0500

    add data clean up

--- src/data_preparation.jl
+++ src/data_preparation.jl
@@ -6,3 +6,15 @@ function load(file_path::AbstractString)
     close(file_stream)
     return structure
 end
+d = size(datapoints, 1)
+data_grouped = Dict{Int32,Vector{Float32}}()
+for (i, a) in enumerate(assignments)
+    vec = get!(data_grouped, a, Float32[])
+    append!(vec, @view datapoints[:, i])
+end
+
+data_grouped_filtered = Dict{Int32, Matrix{Float32}}()
+for (key, vec) in data_grouped
+    (length(vec) <= d) && continue
+    data_grouped_filtered[key] = reshape(vec, d, :)
+end
